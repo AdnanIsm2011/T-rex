c215910d68484b11f4fc982a9481d36d1566051b
AdnanIsm2011 <126856780+AdnanIsm2011@users.noreply.github.com>
2023-03-03 18:18:56 +1100

    Add files via upload

---
+++ Class 9/p5play_template-main/sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(120,123,30,50)
+box.shapeColor="red";
+
+}
+
+function draw() 
+{
+  background(30);
+drawSprites ();
+}
+
+
+
+
